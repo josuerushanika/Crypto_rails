5344c324d46cd71407934002ef7fde4b63f99d3f
josuerushanika <josuerushanika@gmail.com>
2023-07-07 23:12:04 +0200

    Update files

---
+++ db/migrate/20230707211128_create_currencies.rb
@@ -0,0 +1,13 @@
+class CreateCurrencies < ActiveRecord::Migration[7.0]
+  def change
+    create_table :currencies do |t|
+      t.string :name
+      t.string :description
+      t.bigint :maxsupply
+      t.string :currency_symbol
+      t.string :slug
+
+      t.timestamps
+    end
+  end
+end
